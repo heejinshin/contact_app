8583d4871afcd40fd4f6f5787a995482032e3953
Rocat Park <80698977+metayinyang@users.noreply.github.com>
2022-08-11 13:31:02 +0900

    sec

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,109 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Monstereyes());
+  runApp(
+      MaterialApp(
+          home: Monstereyes()
+      )
+  );
 }
 
-class Monstereyes extends StatelessWidget {
-  const Monstereyes({Key? key}) : super(key: key);
+class  Monstereyes extends StatefulWidget {
+  Monstereyes({Key? key}) : super(key: key);
+
+  @override
+  State<Monstereyes> createState() => _MonstereyesState();
+}
+
+class _MonstereyesState extends State<Monstereyes> {
+
+  // States//
+  var total = 3;
+  // final growableList = <String>['A', 'B'];
+  var name = ['김영숙', '홍길동', '피자집'];
+  var like = [0, 0, 0];
+  var inputData = TextEditingController();  // inputData; 사용자가 입력한 데이터를 저장할 변수
+
+
+  //////////////수정함수/////////////
+
+  addOne() {
+    setState((){
+      total++;
+    });
+  }
+
+  addName(inputData) {
+    setState((){
+      name.add(inputData);
+    });
+  }
+///////////////////////////////////
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // scafford 사용해서 top, body, bottom 구분 생성하기
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('앱임'), centerTitle: false,
-          ),
-          body: Text('안녕'),
-            bottomNavigationBar: BottomAppBar(
-                child: SizedBox(
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Icon(Icons.phone),
-                      Icon(Icons.message),
-                      Icon(Icons.contact_page),
-                    ],
-                  ),
-                ),
-              ),
-          )
-    );
+    return Scaffold(
+            floatingActionButton: Builder(
+              builder: (context){
+              return FloatingActionButton(
+              onPressed: (){
+                showDialog(context: context, builder: (context){
+                return DialogUI( inputData:inputData, addOne: addOne,  addName: addName);
+              });
+              },
+              );
+              }
+            ),
+            appBar: AppBar(title: Text(total.toString()),),
+            body: ListView.builder(
+              itemCount: name.length,
+              itemBuilder: (c, i) {
+                return ListTile( // 연락처 여러개 만들기
+                  leading: Image.asset('profile.png', width: 100,),
+                  title: Text(name[i]),
 
+
+                );
+              },
+            )
+        );
   }
 }
+
+
+////////////////////// 커스텀위젯_DialogUI ////////////////
+
+class DialogUI extends StatelessWidget {
+   DialogUI({Key? key, this.inputData, this.addOne, this.addName}) : super(key: key);
+   final addOne;
+   final addName;
+   // input 데이터 관리하기
+  final inputData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField( controller: inputData,),
+            TextButton(child: Text('완료'), onPressed:(){
+              addOne();
+              addName(inputData.text);
+            }),
+            TextButton(child: Text('취소'), onPressed:(){ Navigator.pop(context); })
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:monster_eyes_lock/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Monstereyes());
+    await tester.pumpWidget( Monstereyes());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
